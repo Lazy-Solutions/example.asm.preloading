3d9dc141ad982d9b81cf07bf72a7b5d3b36bcd36
zumwani <andreas.ingeholm@gmail.com>
2021-10-04 20:01:54 +0200

    Fixed example.

---
+++ Assets/Example/Scripts/Door_LevelLoader.cs
@@ -0,0 +1,103 @@
+﻿using AdvancedSceneManager;
+using AdvancedSceneManager.Models;
+using AdvancedSceneManager.Utility;
+using Lazy.Utility;
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Door_LevelLoader : MonoBehaviour
+{
+
+    Vector3 startPos;
+    Vector3 endPos;
+    public Vector3 offset;
+
+    public Scene SceneToLoad;
+
+    public float stayOpenFor;
+
+    private void Start()
+    {
+        startPos = transform.localPosition;
+        endPos = startPos - offset;
+    }
+
+    //Called from TriggerEnterEvent component
+    public void OnPreloadTriggerEnter()
+    {
+        //Check if scene is already open, or is preloaded already,
+        //if not, then we'll start Preload coroutine.
+        var isOpen = SceneManager.utility.IsOpen(SceneToLoad);
+        if (!isOpen && !isOpen.isPreloaded)
+            Preloader.Preload(SceneToLoad).StartCoroutine();
+    }
+
+    bool isInTrigger;
+    private void OnTriggerEnter(Collider other)
+    {
+        isInTrigger = true;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        isInTrigger = false;
+    }
+
+    void Update()
+    {
+
+        if (isInTrigger)
+            if (Preloader.currentState.GetValue(SceneToLoad) == Preloader.LoadState.Waiting)
+                Preloader.FinishLoad(SceneToLoad);
+            else if (Preloader.currentState.GetValue(SceneToLoad) == Preloader.LoadState.Done)
+                OpenDoor();
+    }
+
+    #region Open door
+
+    //Animate door open and close
+
+    bool isAnimating;
+    void OpenDoor()
+    {
+
+        if (isAnimating)
+            return;
+        isAnimating = true;
+
+        TweenOpenDoor(startPos, endPos, () =>
+            countdown(stayOpenFor, () =>
+                TweenOpenDoor(endPos, startPos, () => isAnimating = false).StartCoroutine()).StartCoroutine()
+            ).StartCoroutine();
+
+    }
+
+    IEnumerator TweenOpenDoor(Vector3 startPos, Vector3 endPos, Action done = null)
+    {
+        var t = 0f;
+
+        while (t < 1 / 1)
+        {
+
+            if (!this)
+                yield break;
+
+            transform.localPosition = Vector3.Lerp(startPos, endPos, t);
+            t += Time.deltaTime;
+            yield return null;
+
+        }
+        transform.localPosition = endPos;
+        done?.Invoke();
+    }
+
+    IEnumerator countdown(float time, Action done)
+    {
+        yield return new WaitForSeconds(time);
+        done.Invoke();
+    }
+
+    #endregion
+
+}
---
+++ Assets/Example/Scripts/Preloader.cs
@@ -0,0 +1,42 @@
+﻿using AdvancedSceneManager;
+using AdvancedSceneManager.Models;
+using AdvancedSceneManager.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preloader : MonoBehaviour
+{
+
+    public enum LoadState
+    {
+        None, Preloading, Waiting, QueueFinishLoad, FinishingLoad, Done
+    }
+
+    public static Dictionary<Scene, LoadState> currentState = new Dictionary<Scene, LoadState>();
+
+    public static IEnumerator Preload(Scene scene)
+    {
+
+        currentState.Set(scene, LoadState.Preloading);
+        var async = SceneManager.standalone.Preload(scene);
+        yield return async;
+
+        if (currentState.GetValue(scene) != LoadState.QueueFinishLoad)
+        {
+            currentState.Set(scene, LoadState.Waiting);
+            yield return new WaitUntil(() => currentState.GetValue(scene) == LoadState.QueueFinishLoad);
+        }
+
+        currentState.Set(scene, LoadState.FinishingLoad);
+        yield return async.value.FinishLoading();
+        currentState.Set(scene, LoadState.Done);
+
+    }
+
+    public static void FinishLoad(Scene scene)
+    {
+        currentState.Set(scene, LoadState.QueueFinishLoad);
+    }
+
+}
